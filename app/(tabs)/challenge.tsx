a9c3c8d49c6169205df7b65d4afa14901401017c
yeeun426 <thsudkcla7@naver.com>
2025-06-18 15:35:28 +0900

    Feat: [통합 관리] route 정리 (MOYEO-30)

--- app/(tabs)/challenge.tsx
+++ app/(tabs)/challenge.tsx
@@ -1,20 +1,40 @@
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function challenge() {
+  const goToMakeChallenge = () => {
+    router.push("/make-challenge");
+  };
+
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.wrapper}>
         <View style={styles.header}>
-          <Ionicons name="chevron-back" size={20} color="orange" />
+          <Ionicons name="chevron-back" size={22} />
           <Text style={styles.title}>챌린지</Text>
-          <Ionicons name="search-outline" size={20} color="#BABABA" />
-          <Ionicons name="people-outline" size={20} color="#BABABA" />
+          <Ionicons name="search-outline" size={22} color="#BABABA" />
+          <Ionicons name="people-outline" size={22} color="#BABABA" />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Search Challenge"
+            placeholderTextColor="#878787"
+            style={styles.input}
+          />
+          <Ionicons name="search-outline" size={18} color="black" />
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="orange" />
+          <Ionicons name="calendar" size={22} color="orange" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -27,7 +47,7 @@ export default function challenge() {
           </View>
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="#BABABA" />
+          <Ionicons name="calendar" size={22} color="#BABABA" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -40,7 +60,7 @@ export default function challenge() {
           </View>
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="#BABABA" />
+          <Ionicons name="calendar" size={22} color="#BABABA" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -53,7 +73,7 @@ export default function challenge() {
           </View>
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="#BABABA" />
+          <Ionicons name="calendar" size={22} color="#BABABA" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -66,7 +86,7 @@ export default function challenge() {
           </View>
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="#BABABA" />
+          <Ionicons name="calendar" size={22} color="#BABABA" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -79,7 +99,7 @@ export default function challenge() {
           </View>
         </View>
         <View style={styles.challengeCard}>
-          <Ionicons name="calendar" size={20} color="#BABABA" />
+          <Ionicons name="calendar" size={22} color="#BABABA" />
           <View style={styles.challengeContent}>
             <View>
               <Text style={styles.subText}>5.15 ~ 8.15</Text>
@@ -92,12 +112,16 @@ export default function challenge() {
           </View>
         </View>
       </ScrollView>
+      <TouchableOpacity style={styles.fixedButton} onPress={goToMakeChallenge}>
+        <Text style={styles.buttonText}>Add Challenge</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
+    paddingBottom: 100,
     padding: 20,
     backgroundColor: "#fff",
   },
@@ -111,6 +135,28 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
     marginHorizontal: 5,
+  },
+  inputContainer: {
+    height: 55,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 6, height: 6 },
+    shadowOpacity: 0.08,
+    shadowRadius: 60,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#EAEAEA",
+  },
+  input: {
+    flex: 1,
+    fontSize: 14,
+    color: "#000",
+  },
+  challengeSub: {
+    textAlign: "center",
   },
   title: {
     flex: 1,
@@ -136,7 +182,10 @@ const styles = StyleSheet.create({
   challengeContent: {
     display: "flex",
     flexDirection: "row",
-    alignContent: "space-between",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flex: 1,
+    marginLeft: 10,
   },
   calendarIcon: {
     width: 24,
@@ -144,17 +193,39 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
   titleText: {
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: "600",
     color: "#24252C",
   },
   subText: {
-    fontSize: 9,
+    fontSize: 12,
     color: "#6E6A7C",
+    marginBottom: 3,
   },
   countText: {
     fontSize: 14,
     fontWeight: "600",
     color: "#24252C",
+    textAlign: "center",
+  },
+  fixedButton: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: "#FF6A00",
+    paddingVertical: 16,
+    borderRadius: 12,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
